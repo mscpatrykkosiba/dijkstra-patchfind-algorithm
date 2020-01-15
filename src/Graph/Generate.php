b90920215673fb42c25bbcf6a143f7f977a790c2
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2020-01-15 17:29:53 +0100

    Update Generate.php

--- src/Graph/Generate.php
+++ src/Graph/Generate.php
@@ -64,58 +64,57 @@
          */
         public function loadFromFile($file){
             
-            $res = imagecreatefromjpeg($file);
-            if($res){
+            $res = @imagecreatefromjpeg($file);
+           
+            if(!$res){
                 $width = imagesx($res);
                 $height = imagesy($res);
-            }else{
-                $width = 100;
-                $height = 100;
-            }
-            
-            $new_tab = array();
-            for($y = 0; $y <= $height; $y++){
-                for($x = 0; $x <= $width; $x++){
-                    if($y==$height || $x==$width){
-                        $color = 10;
-                    }else{
-                        $color = imagecolorat($res, $x, $y);
-                    }
-                    $new_tab[$x][$y] = $color;
-                }
-            } 
-            
-            $this->x_w = $width;
-            $this->y_h = $height;
-            
-            $modX = ceil($width/2);
-            $modY = ceil($height/2);
 
-            $relations = array();
-            for($y = 0; $y <= $height; $y++){
-                for($x = 0; $x <= $width; $x++){
-                    
-                    if($new_tab[$x][$y]<=100){
-                       $k3 = 9999; 
-                    }else{
-                        $k = $x; $k2 = $y;
-                        if($x<$modX) $k = $width-$x;
-                        if($y<$modY) $k2 = $height-$y;
-                        $k3 = $k;
-                        if($k2>$k) $k3 = $k2;
+                $new_tab = array();
+                for($y = 0; $y <= $height; $y++){
+                    for($x = 0; $x <= $width; $x++){
+                        if($y==$height || $x==$width){
+                            $color = 10;
+                        }else{
+                            $color = imagecolorat($res, $x, $y);
+                        }
+                        $new_tab[$x][$y] = $color;
                     }
-                    $tmpX = $x-1; $tmpY = $y;
-                    if($this->checkParams($tmpX, $tmpY)) $relations["($tmpX,$tmpY)"]["($x,$y)"] = $k3;
-                   
-                    $tmpX2 = $x+1; $tmpY2 = $y;
-                    if($this->checkParams($tmpX2, $tmpY2)) $relations["($tmpX2,$tmpY2)"]["($x,$y)"] = $k3;
+                } 
 
-                    $tmpX3 = $x; $tmpY3 = $y-1;
-                    if($this->checkParams($tmpX3, $tmpY3)) $relations["($tmpX3,$tmpY3)"]["($x,$y)"] = $k3;
-                    
-                    $tmpX4 = $x; $tmpY4 = $y+1;
-                    if($this->checkParams($tmpX4, $tmpY4)) $relations["($tmpX4,$tmpY4)"]["($x,$y)"] = $k3;
+                $this->x_w = $width;
+                $this->y_h = $height;
+
+                $modX = ceil($width/2);
+                $modY = ceil($height/2);
+
+                $relations = array();
+                for($y = 0; $y <= $height; $y++){
+                    for($x = 0; $x <= $width; $x++){
+
+                        if($new_tab[$x][$y]<=100){
+                           $k3 = 9999; 
+                        }else{
+                            $k = $x; $k2 = $y;
+                            if($x<$modX) $k = $width-$x;
+                            if($y<$modY) $k2 = $height-$y;
+                            $k3 = $k;
+                            if($k2>$k) $k3 = $k2;
+                        }
+                        $tmpX = $x-1; $tmpY = $y;
+                        if($this->checkParams($tmpX, $tmpY)) $relations["($tmpX,$tmpY)"]["($x,$y)"] = $k3;
+
+                        $tmpX2 = $x+1; $tmpY2 = $y;
+                        if($this->checkParams($tmpX2, $tmpY2)) $relations["($tmpX2,$tmpY2)"]["($x,$y)"] = $k3;
+
+                        $tmpX3 = $x; $tmpY3 = $y-1;
+                        if($this->checkParams($tmpX3, $tmpY3)) $relations["($tmpX3,$tmpY3)"]["($x,$y)"] = $k3;
+
+                        $tmpX4 = $x; $tmpY4 = $y+1;
+                        if($this->checkParams($tmpX4, $tmpY4)) $relations["($tmpX4,$tmpY4)"]["($x,$y)"] = $k3;
+                    }
                 }
+                $relations = $res;
             }
             
             return $relations;
