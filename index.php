b5f42d1169f26144e24db31c8b4ea67635dd816e
MSc Patryk Kosiba <patryk.kosiba@gmail.com>
2020-01-15 17:09:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,8 @@
         
         $output = new PatchFind\Calculate();
         
-        //set start position
         $output->setStartXY(0,0);
-        
-        //set finish position
         $output->setEndXY(99,99);
-        
-        //create graph
         $graph = $output->makeGraph(100,100);
 
 
